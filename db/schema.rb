4a0ed50fd118452728990117ed7cb7d7970467ef
3sho7mi8 <hiro.pinot@gmail.com>
2017-11-07 16:30:28 +0900

    Add column "url" to GalleriesTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107012817) do
+ActiveRecord::Schema.define(version: 20171107072651) do
 
   create_table "galleries", force: :cascade do |t|
     t.string "name"
     t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
 end
