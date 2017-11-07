48e3272041b3a5c5f149da0cb20473be685654db
3sho7mi8 <hiro.pinot@gmail.com>
2017-11-07 11:01:00 +0900

    Add test to Gallery Model

--- test/models/gallery_test.rb
+++ test/models/gallery_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class GalleryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    note_text = <<~EOM
+      Location is Japan.
+      There is a tradition since 1900.
+      But this gallery is for testing.
+      do not exist.
+    EOM
+    @gallery = Gallery.new(name: 'Example Gallery', note: note_text)
+  end
+
+  test "should be valid" do
+    assert @gallery.valid?
+  end
+
+  test "name should be present" do
+    @gallery.name = ""
+    assert_not @gallery.valid?
+  end
 end
